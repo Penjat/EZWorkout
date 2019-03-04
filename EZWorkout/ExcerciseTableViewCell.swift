f7a023452c42214f606a5e185d07efcd2076a774
28cmm <joshuafang1998@gmail.com>
2019-03-04 12:49:13 -0800

    the first layout without git ignore

---
+++ EZWorkout/ExcerciseTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  ExcerciseTableViewCell.swift
+//  EZWorkout
+//
+//  Created by Yilei Huang on 2019-03-01.
+//  Copyright © 2019 Spencer Symington. All rights reserved.
+//
+
+import UIKit
+
+class ExcerciseTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
