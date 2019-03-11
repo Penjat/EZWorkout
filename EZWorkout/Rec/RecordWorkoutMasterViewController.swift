61d7f63ef0f07daca776681681c328cf36076fe6
Penjat <theSpencerSymington@Gmail.com>
2019-03-10 17:05:32 -0700

    minor color change

--- EZWorkout/Rec/RecordWorkoutMasterViewController.swift
+++ EZWorkout/Rec/RecordWorkoutMasterViewController.swift
@@ -55,8 +55,8 @@ class RecordWorkoutMasterViewController: UIViewController {
     
     gradient.colors = [idleColor1.cgColor, idleColor2.cgColor]
     gradient.locations = [0.0 , 1.0]
-    gradient.startPoint = CGPoint(x: 0.0, y: 1.0)
-    gradient.endPoint = CGPoint(x: 1.0, y: 1.0)
+    gradient.startPoint = CGPoint(x: 0.0, y: 0.0)
+    gradient.endPoint = CGPoint(x: 1.0, y: 0.0)
     
     gradient.frame = CGRect(x: 0.0, y: 0.0, width: self.view.frame.size.width, height: self.view.frame.size.height)
     self.view.layer.insertSublayer(gradient, at: 0)
