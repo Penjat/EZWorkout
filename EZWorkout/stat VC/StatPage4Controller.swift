4753699ab2fc258b3cb95cd41e99ef435fe72d9a
Penjat <theSpencerSymington@Gmail.com>
2019-03-11 17:02:13 -0700

    charts mostly positioned

---
+++ EZWorkout/stat VC/StatPage4Controller.swift
@@ -0,0 +1,30 @@
+//
+//  StatPage4Controller.swift
+//  EZWorkout
+//
+//  Created by Spencer Symington on 2019-03-11.
+//  Copyright © 2019 Spencer Symington. All rights reserved.
+//
+
+import UIKit
+
+class StatPage4Controller: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
