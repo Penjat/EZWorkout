5d1a54a4a3e8bc74da2a46ebfe2bfac6addeea45
Penjat <theSpencerSymington@Gmail.com>
2019-03-11 18:10:02 -0700

    all data added to graphs

--- EZWorkout/stat VC/stats pages/StatPage4Controller.swift
+++ EZWorkout/stat VC/stats pages/StatPage4Controller.swift
@@ -1,30 +1,84 @@
-//
-//  StatPage4Controller.swift
-//  EZWorkout
-//
-//  Created by Spencer Symington on 2019-03-11.
-//  Copyright © 2019 Spencer Symington. All rights reserved.
-//
+
 
 import UIKit
+import Charts
+import UICircularProgressRing
 
 class StatPage4Controller: UIViewController {
+  
+  @IBOutlet weak var chartCaloryGoal: UICircularProgressRing!
 
+  @IBOutlet weak var chartTimeSpent: BarChartView!
+  
+  @IBOutlet weak var chartCaloriesBurned: BarChartView!
+  
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+      setupChart2()
+      setupChart3()
     }
+  
+  
+  func setupChart2(){
     
+    print(UserRealm.curUser.name)
+    let exerciseDict = StaticDataManager.getRealmData(type: .numberOfTimes)
+    print("this  is a dictionary of  \(exerciseDict.count) ")
+    let topSevenDict = StaticDataManager.sortRealmDict(Dict: exerciseDict)
+    print("this is top 7 sport \(topSevenDict)")
+    
+    
+    let barColor = #colorLiteral(red: 0.9428298473, green: 0.9622165561, blue: 0, alpha: 1)
+    
+    StaticDataManager.setUpChart(chart: chartCaloriesBurned , chartLabels: topSevenDict.map{$0.0} , chartValues: topSevenDict.map{$0.1} , colors: [barColor],bottomLabelText: "Weigth")
+    
+    //      chart1.leftAxis.labelFont = UIFont(name: "Simply Rounded", size: 20.0)!
+    chartCaloriesBurned.xAxis.labelFont  = UIFont(name: "Simply Rounded", size: 20.0)!
+    
+    chartCaloriesBurned.legend.font = UIFont(name: "Simply Rounded", size: 20.0)!
+    
+    
+    //      chart1.legend.formSize = 20.0
+    //      chart1.legend.yOffset = 20.0
+    //      chart1.legend.neededHeight = 50
+    //chart1.legend.enabled = false
+    
+    
+    
+    
+  }
+  
+  func setupChart3(){
+    
+    print(UserRealm.curUser.name)
+    let exerciseDict = StaticDataManager.getRealmData(type: .numberOfTimes)
+    print("this  is a dictionary of  \(exerciseDict.count) ")
+    let topSevenDict = StaticDataManager.sortRealmDict(Dict: exerciseDict)
+    print("this is top 7 sport \(topSevenDict)")
+    
+    
+    let barColor = #colorLiteral(red: 0.9428298473, green: 0.9622165561, blue: 0, alpha: 1)
+    
+    StaticDataManager.setUpChart(chart: chartTimeSpent , chartLabels: topSevenDict.map{$0.0} , chartValues: topSevenDict.map{$0.1} , colors: [barColor],bottomLabelText: "Weigth")
+    
+    //      chart1.leftAxis.labelFont = UIFont(name: "Simply Rounded", size: 20.0)!
+    chartTimeSpent.xAxis.labelFont  = UIFont(name: "Simply Rounded", size: 20.0)!
+    
+    chartTimeSpent.legend.font = UIFont(name: "Simply Rounded", size: 20.0)!
+    
+    
+    //      chart1.legend.formSize = 20.0
+    //      chart1.legend.yOffset = 20.0
+    //      chart1.legend.neededHeight = 50
+    //chart1.legend.enabled = false
+    
+    
+    
+    
+  }
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
+  
 
 }
